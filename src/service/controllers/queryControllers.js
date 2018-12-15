db24ac8b16a26e464af2582fd2494a2ead3bf794
Claudio De Angelis <claudio.deangelis@cloud-iq.com>
2018-12-15 19:01:11 +1100

    working prototype

--- src/service/controllers/queryControllers.js
+++ src/service/controllers/queryControllers.js
@@ -11,7 +11,7 @@ const insertProduct = async (product) => {
             dateCreated: new Date(),
         });
         productDetails.cloudiqProductPrice = parseFloat(productDetails.cloudiqProductPrice);
-        const query = await getDB().collection('products').insertOne(productDetails);
+        const query = await getDB().collection(product.cloudiqProductDomain).insertOne(productDetails);
         if(query.result.ok === 1 && query.result.n === 1){
             console.log('\n\n-> Product Inserted !!\n');
             return true;
@@ -32,7 +32,7 @@ const compareProduct = async (product) => {
         const price = (parseFloat(product.cloudiqProductPrice) * 51.85 - 1).toString().split('.')[0];
         const url = `https://price-api.datayuge.com/api/v1/compare/search?api_key=CpNdKIt2fovc1KC1oszOftIwsYE0QLNu8jU&product=${product.cloudiqProductName}&filter=brand%3A${product.cloudiqProductBrand}&price_start=0&price_end=${price}&page=1`;
         const res = await axios.get(url);
-        console.log(`-> Found${res.data.data && res.data.data.length === 50 ? ` at least` : ``} ${res.data.data && res.data.data.length} cheaper items with the comparison API\n`);
+        console.log(`-> Found${res.data.data && typeof res.data.data !== 'string' && res.data.data.length === 50 ? ` at least` : ``} ${res.data.data && res.data.data.length} cheaper items with the comparison API\n`);
         if (res.data.data && res.data.data.length > 0) {
             return {isBestPrice: 'false'};
         }
@@ -45,17 +45,24 @@ const compareProduct = async (product) => {
 };
 
 const findCheaperLocalProduct = async (product) => {
-    console.log('-> Now Looking for cheapert local items...\n');
+    console.log(`-> Now Looking for local items cheaper than ${product.cloudiqProductName}...\n`);
     try{
-        const query = parser(`{"cloudiqProductPrice": {$lt : ${product.cloudiqProductPrice}}}`);
-        const result = await getDB().collection('products').findOne(query);
+        const query = parser(`
+            {
+                "cloudiqProductName": {$not: /${product.cloudiqProductName}/},
+                "cloudiqProductCategory":"${product.cloudiqProductCategory}",
+                "cloudiqProductPrice": {$lt : ${product.cloudiqProductPrice}}
+            }
+        `);
+        const result = await getDB().collection(product.cloudiqProductDomain).findOne(query);
         if(result) {
             console.log('-> Found!\n');
             result.cloudiqProductPrice = result.cloudiqProductPrice.toFixed(2);
             result.isBestPrice= 'false';
             return result;
         } else {
-            return {};
+            console.log('-> None Found!\n');
+            return {isBestPrice: 'true'};
         }
     }catch(err){
         console.log('INDEX ::findCheaperLocalProduct: ', err);
